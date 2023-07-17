b4cf4620cc22250fe7f0f02b1b955e74409730c6
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-07-18 02:42:28 +0900

    ジャンプの改良 （まだ試行錯誤中）

--- Assets/Script/PlayerDef.cs
+++ Assets/Script/PlayerDef.cs
@@ -11,17 +11,18 @@ public class PlayerDef : MonoBehaviour
     [SerializeField] bool  _isGrounded = true;
     [SerializeField] int _jumpcount = 0;
     [SerializeField] int _maxJumpCount = 2;
+    [SerializeField] float _downSpeed = 50;
 
     Rigidbody2D _rb2d;
     Animation _animator;
 
     float h = 0;
+    float v = 0;
 
 
     void Start()
     {
         _rb2d = GetComponent<Rigidbody2D>();
-
         _animator = GetComponent<Animation>();
     }
     void Update()
@@ -33,16 +34,24 @@ public class PlayerDef : MonoBehaviour
      public void Movement()
     {
         h = Input.GetAxis("Horizontal");
+        //v = Input.GetAxis("Velocity");
 
         Vector2 velocity = new Vector2(h, 0) * _speed;
         _rb2d.velocity = velocity;
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            _rb2d.AddForce(Vector2.down * _downSpeed);
+        }
     }
 
     public void Jump()
     {
         if (Input.GetKeyDown(KeyCode.Space) && _maxJumpCount >= _jumpcount)
         {
-            _rb2d.velocity = new Vector2(h, _jumpForce * _speed);
+            _rb2d.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+
+            //_rb2d.velocity = new Vector2(h, _jumpForce * _speed);
             _isGrounded = false;
             _jumpcount++;
         }
