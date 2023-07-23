4c4ba2325daf4b1c4fa67ef4f4f3bcf75914e978
yuya's <134368817+ShinchiYuya@users.noreply.github.com>
2023-07-23 14:32:41 +0900

    〃

--- Assets/Script/PullPlayer.cs
+++ Assets/Script/PullPlayer.cs
@@ -5,17 +5,41 @@ using UnityEngine;
 public class PullPlayer : PlayerDefScript
 {
     // Start is called before the first frame update
-    protected override void Start()
+    new void Start()
     {
         base.Start();
     }
 
     // Update is called once per frame
-    protected override void Update()
+    new void Update()
     {
         base.Update();
 
         // Your additional logic for grabbing and impulse force here...
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            // Grab logic here
+            GameObject[] grabbableObjects = GameObject.FindGameObjectsWithTag("GrabbableObject");
+            if (grabbableObjects.Length > 0)
+            {
+                GameObject objToGrab = grabbableObjects[0];
+                GrabObject(objToGrab);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            // Impulse force logic here
+            GameObject[] pushableObjects = GameObject.FindGameObjectsWithTag("PushableObject");
+            if (pushableObjects.Length > 0)
+            {
+                GameObject objToPush = pushableObjects[0];
+                Vector2 forceDirection = Vector2.up; // Set the desired force direction (you can change this as needed)
+                float forceMagnitude = 10f; // Set the desired force magnitude (you can change this as needed)
+                ImpulseForce(objToPush, forceDirection, forceMagnitude);
+            }
+        }
     }
 
     protected override void ImpulseForce(GameObject objToPush, Vector2 forceDirection, float forceMagnitude)
